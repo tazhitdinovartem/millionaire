baee674c2a6292a20f31d761c95fd7d9eb7db8c2
artem.aspire <nemesisrise@gmail.com>
2020-03-17 13:46:06 +0500

    Rspec test for GameQuestion#friend_call

--- spec/models/game_question_spec.rb
+++ spec/models/game_question_spec.rb
@@ -64,5 +64,15 @@ RSpec.describe GameQuestion, type: :model do
       expect(ff).to include("b")
       expect(ff.size).to eq(2)
     end
+
+    it 'correct correct friend_call' do
+      expect(game_question.help_hash).not_to include(:friend_call)
+
+      game_question.add_friend_call
+
+      expect(game_question.help_hash).to include(:friend_call)
+      fc = game_question.help_hash[:friend_call]
+      expect(fc).to include('считает, что это вариант')
+    end
   end
 end
