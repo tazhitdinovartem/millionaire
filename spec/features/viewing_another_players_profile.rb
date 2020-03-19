239e9eb6b54e227d347f1e84c11ae3c2b1474aa4
ArtemTazhitdinov <tazhitdinov.artem@gmail.com@>
2020-03-19 08:57:24 +0500

    fix for rspec features

--- spec/features/viewing_another_players_profile.rb
+++ spec/features/viewing_another_players_profile.rb
@@ -1,43 +1,45 @@
 require 'rails_helper'
 
 RSpec.feature 'USER viewing another players profile', type: :feature do
-  user1 = FactoryBot.create(:user, name: 'user1', id: 1) 
-  user2 = FactoryBot.create(:user, name: 'user2', id: 2) 
+  let(:user_first) { FactoryBot.create(:user, name: 'user_first', id: 1) } 
+  let(:user_second) { FactoryBot.create(:user, name: 'user_second', id: 2) } 
 
-  FactoryBot.create(:game_with_questions, 
-    user: user2, 
-    current_level: 5, 
+  let!(:Game_One) { FactoryBot.create(:game_with_questions,
+    id: 13,
+    user: user_second, 
+    current_level: 1, 
     prize: 250,
-    created_at: Time.parse('2020-03-18 13:00')
-  )
-  FactoryBot.create(:game_with_questions, 
-    user: user2, 
+    created_at: Time.parse('2020-01-01 8:00')
+  )}
+  let!(:Game_Two) { FactoryBot.create(:game_with_questions,
+    id: 14,
+    user: user_second, 
     current_level: 6, 
     prize: 3000, 
     created_at: Time.parse('2020-03-17 13:00'), 
     finished_at: Time.parse('2020-03-17 13:20'),
     is_failed: true
-  )
+  )}
 
   before(:each) do
-    login_as user1
+    login_as user_first
   end
 
   scenario 'successfully' do
     visit '/'
-    click_link 'user2'
+    click_link 'user_second'
 
     expect(page).to have_current_path '/users/2'
-    expect(page).to have_content 'user2'
+    expect(page).to have_content 'user_second'
     expect(page).to have_no_content 'Сменить имя и пароль'
 
-    expect(page).to have_content '1'
+    expect(page).to have_content '13'
     expect(page).to have_content 'в процессе'
-    expect(page).to have_content '18 марта, 13:00'
-    expect(page).to have_content '5'
+    expect(page).to have_content '01 янв., 08:00'
+    expect(page).to have_content '1'
     expect(page).to have_content '250'
 
-    expect(page).to have_content '2'
+    expect(page).to have_content '14'
     expect(page).to have_content 'проигрыш'
     expect(page).to have_content '17 марта, 13:00'
     expect(page).to have_content '6'
