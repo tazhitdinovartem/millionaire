8e763b82e14b8f9d220a86f3081998eaa44a81f1
ArtemTazhitdinov <tazhitdinov.artem@gmail.com@>
2020-03-19 14:32:31 +0500

    Imporove unit tests

--- spec/views/users/show.html.erb_spec.rb
+++ spec/views/users/show.html.erb_spec.rb
@@ -1,22 +1,46 @@
 require 'rails_helper'
 
 RSpec.describe 'users/show', type: :view do
-  before(:each) do
-    user = assign(:user, FactoryBot.create(:user, name: 'UserForTest'))
-    game = assign(:games, [FactoryBot.create(:game_with_questions)])
-    sign_in user
-    render
+  context 'user watches another users profile' do
+    before(:each) do
+      assign(:user, FactoryBot.create(:user, name: 'UserForTest'))
+      assign(:games, [FactoryBot.create(:game_with_questions)])
+      
+      render
+    end
+
+    it 'renders user name' do
+      expect(rendered).to match 'UserForTest'
+    end
+
+    it 'does not renders edit-password button only for current user' do
+      expect(rendered).not_to match 'Сменить имя и пароль'
+    end
+
+    it 'renders users games table' do
+      render_template partial: 'users/_game'
+    end
   end
 
-  it 'renders user name' do
-    expect(rendered).to match 'UserForTest'
-  end
+  context 'user watches his own profile' do
+    before(:each) do
+      user = assign(:user, FactoryBot.create(:user, name: 'UserForTest'))
+      assign(:games, [FactoryBot.create(:game_with_questions)])
+      sign_in user
 
-  it 'renders edit-password button only for current user' do
-    expect(rendered).to match 'Сменить имя и пароль'
-  end
+      render
+    end
 
-  it 'renders users games table' do
-    render_template partial: 'users/_game'
+    it 'renders user name' do
+      expect(rendered).to match 'UserForTest'
+    end
+
+    it 'renders edit-password button only for current user' do
+      expect(rendered).to match 'Сменить имя и пароль'
+    end
+
+    it 'renders users games table' do
+      render_template partial: 'users/_game'
+    end
   end
 end
